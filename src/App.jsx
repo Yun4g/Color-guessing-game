e913dc3ef8357d3b94e6cb1274b2e49e0dde5da2
Yun4g <delightvincent487@gmail.com>
2025-02-05 09:20:46 +0100

    fixed rsponsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -77,15 +77,15 @@ function App() {
  
 
   return (
-    <section className=' flex justify-center items-center bg-slate-200   h-screen '>
-        <div className=' overflow-x-hidden md:rounded-lg h-full md:h-[90vh]  md:shadow-lg shadow-cyan-700   lg:w-[700px] '>
-          <div className=' bg-cyan-500 text-white p-4  w-full text-center '>
+    <section className=' lg:flex justify-center  items-center bg-slate-200  lg:h-screen  '>
+        <div className=' flex items-center flex-col justify-center   lg:rounded-lg h-full  lg:h-4/6   w-full  lg:w-[700px] '>
+          <div className=' bg-cyan-500 text-white p-4   w-full text-center '>
           <h1 className='  text-xl  md:text-3xl  font-semibold'>Welcome to the HNG Color Guessing Game</h1>
          
              
           </div>
          
-          <div className=" h-fit lg:h-11/12 flex flex-col justify-center  rounded-b-lg  items-center p-3  bg-gray-700">
+          <div className="  flex flex-col justify-center  lg:rounded-b-lg    items-center p-3  w-full bg-gray-700">
 
             <div data-testid="gameInstructions" className=' text-white px-9 md:px-0 mb-3 w-96 mt-8 text-center'>
                <h2>
@@ -96,27 +96,29 @@ function App() {
  
               </p>
             </div>
+            <div className='flex flex-col lg:flex-row justify-center items-center gap-3 w-full'>
             {
             correctColor && (
-                <div style={{ backgroundColor: `rgb(${correctColor})` }} className="correct-color-container transition-colors duration-700 relative  flex justify-center items-center  h-72 w-96 rounded-lg bg-white">
-               
+                <div style={{ backgroundColor: `rgb(${correctColor})` }} className="correct-color-container transition-colors duration-700 relative  flex justify-center items-center  h-48   w-9/12 md:w-1/2 rounded-lg bg-white">
+                 
                 </div>
   
             )
            }
-            <div className=' w-full grid grid-cols-2 md:grid-cols-3 gap-4 p-4 '>
+            <div className=' w-80 md:w-full grid grid-cols-2 md:grid-cols-3 gap-2 md:gap-3 p-4 md:p-0  '>
             {colorOption.map((color, index) => (
               <button data-testid="colorOption"  key={index} className=' h-28 rounded-lg hover:opacity-75   transition-colors duration-700' style={{ backgroundColor: `rgb(${color})` }} onClick={() => handleClick(color)}>
                 ??
               </button>
             ))}   
             </div>
+            </div>
 
-            <h3 data-testid="gameStatus" className=' text-xl text-white'>{message}</h3>
+            <h3 data-testid="gameStatus" className=' text-xl mt-4 text-white'>{message}</h3>
              <button data-testid="score" className=' text-white text-2xl'>
             score: {score}
           </button>
-          <button data-testid="newGameButton" className=' p-4 text-xl mb-4 bg-neutral-400 rounded-xl' onClick={startNewGame}> Start New Game</button>
+          <button data-testid="newGameButton" className=' p-3 text-xl mb-4 bg-neutral-400 rounded-xl' onClick={startNewGame}> Start New Game</button>
           </div>
          
         </div>
